9e0b2b2df60bb31b14e61a41299d1a9aea070c17
bomma <abhishekreddybomma@gmail.com>
2021-03-04 22:16:49 -0800

    nav bar till pat 10

--- app/src/main/java/com/example/expense_tracer/MainActivity.java
+++ app/src/main/java/com/example/expense_tracer/MainActivity.java
@@ -7,6 +7,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,6 +20,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG="Expense Tracer";
     private EditText mEmail;
     private EditText mpass;
     private Button btnLogin;
@@ -69,8 +71,15 @@ public class MainActivity extends AppCompatActivity {
                     if(task.isSuccessful())
                     {
                         mDialog.dismiss();
-                        startActivity(new Intent(getApplicationContext(), HomeActivity.class));
-                        Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        try {
+                            startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                        }catch (Exception ex){
+                            Log.d(TAG,ex.getMessage());
+                        }
+
+
+
 
                     }
                     else
